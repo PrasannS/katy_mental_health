08d64b4b9af81b5e16700102c6c1308dbce00ba4
lucaszcai <lucas.z.cai@gmail.com>
2019-10-28 23:09:38 -0500

    added floating + button

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,13 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         controller: _tabController,
         children: _tabList,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        elevation: 2.0,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.redAccent,
         selectedItemColor: Colors.blue,
@@ -105,6 +112,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
           )
         ],
       ),
+
     );
   }
 }
